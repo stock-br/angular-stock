1e3a191d3670bdfb550153dc58a01e66fb66afd4
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2022-05-30 17:18:51 -0300

    chore: change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://10.147.20.49:5000/api'
-  baseUrl: process.env['BASE_URL']
-    ? process.env['BASE_URL']
-    : 'http://localhost:5000/api',
+  baseUrl: 'https://stock-br.herokuapp.com/api'
 };
